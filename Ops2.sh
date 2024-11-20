99fdc6b844f6f322375465da1f474276a69fcbec
Ashley <ashley@kali.savvy>
2024-11-19 19:04:15 -0600

    Name variable declared and printed.

---
+++ Ops2.sh
@@ -0,0 +1,12 @@
+$ops2.sh
+
+# #!bin/bash
+# Declare a variable with as your name
+# Print to the screen Welcome to the Class Name(variable)
+
+name="Ashley"
+dog="Jax"
+car="Volkswagon"
+
+
+echo "Welcome to the class, $name. Whats your dogs name? $dog. What kind of car do you drive? $car"
