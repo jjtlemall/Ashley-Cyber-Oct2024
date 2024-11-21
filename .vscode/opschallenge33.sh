7242d9d990275bae99e4af6694d7602ae537987c
jjtlemall <shelton.a0417@gmail.com>
2024-11-20 19:05:53 -0600

    ops challenge 3 completed

---
+++ .vscode/opschallenge33.sh
@@ -0,0 +1,13 @@
+# #!bin/bash
+# Declare a variable with as your name
+# Print to the screen Welcome to the Class Name(variable)
+
+number= 23
+if {$"number" -gt 5}
+  then
+echo "greater than 5"
+  else
+echo "equal to 5"
+fi
+
+
