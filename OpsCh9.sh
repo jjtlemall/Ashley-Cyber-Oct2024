b33bf3a2355201dd86913b28439f11e6a39ae06c
Ashley <ashley@kali.savvy>
2024-12-03 18:58:47 -0600

    added counter loop

--- OpsCh9.sh
+++ OpsCh9.sh
@@ -4,10 +4,15 @@
 #Somebody that had a control of a bot net could set up this script on thousands of computer and ping sites till they are overloaded and crash
 
 while [ true ]; do
-  ping "8.8.8.8"
+    ping "8.8.8.8"
 done
 # To End the loop try pressing control z or control c
 # Stretch Goal:
 # Can you do this with an until loop to have it execute a specfic number of times?
 
+counter=0
 
+until [ $counter -gt 5 ];do
+    echo $counter
+    ((counter++))
+done
